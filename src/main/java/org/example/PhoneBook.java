d2100421f55a4e251435341af49a6f367d68b84d
Yulia Nikolaeva <julia.nikolaeva@live.com>
2024-07-31 00:25:52 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private final Map<String, String> entries;
+
+    public PhoneBook() {
+        entries = new HashMap<>();
+    }
+
     public int add(String name, String number) {
-        return 0;
+        entries.put(name, number);
+        return entries.size();
     }
 }
